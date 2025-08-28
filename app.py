1b966ccd1ce197afbec45cde9754b41fd239a285
xinleiyo <yoyochen2000@gmail.com>
2025-08-28 16:32:36 +0800

    1

--- app.py
+++ app.py
@@ -851,5 +851,5 @@ def uploaded_file(filename):
 # ===============================================
 # 伺服器運行
 # ===============================================
-# if __name__ == '__main__':
-#         app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000, debug=True)
