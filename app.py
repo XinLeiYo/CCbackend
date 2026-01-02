ce86e92e195c677516b208b27ad586508d0e2865
xinleiyo <yoyochen2000@gmail.com>
2026-01-02 21:30:29 +0800

    port update

--- app.py
+++ app.py
@@ -894,4 +894,6 @@ def uploaded_file(filename):
 # 伺服器運行
 # ===============================================
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5432, debug=True)
+        import os
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host='0.0.0.0', port=port)
